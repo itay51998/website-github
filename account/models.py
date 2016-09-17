e2cf14101913dc62d51e1631d2ba471a4dcd4860
itay51998 <itay51998@gmail.com>
2016-09-17 21:00:50 +0300

    stuff
    new

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    username = models.CharField(max_length=16)
+    nickname = models.CharField(max_length=16)
+    email = models.EmailField(max_length=45)
+    password = models.CharField(max_length=32)
+    style = models.UUIDField(primary_key=False)
+    rating = models.DecimalField(max_digits=1, decimal_places=1)
--- website/settings.py
+++ website/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
